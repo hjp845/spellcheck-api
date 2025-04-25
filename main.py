90905e90a0bc25e46c72483d02151373b424a0fb
hjp845 <tlfgql@dgist.ac.kr>
2025-04-25 14:04:42 +0900

    502handle3

--- main.py
+++ main.py
@@ -3,21 +3,20 @@ from flask_cors import CORS
 from hanspell import spell_checker
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/check": {"origins": "*"}}, supports_credentials=True)
 
 @app.route('/check', methods=['POST', 'OPTIONS'])
 def check():
     if request.method == 'OPTIONS':
-        response = make_response()
+        response = make_response('', 200)
         response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        return response, 200  # ✅ 200 명시!
+        return response
 
     data = request.get_json()
     text = data.get("text", "")
     res = spell_checker.check(text)
-
     response = jsonify({"result": res.checked})
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
